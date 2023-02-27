6aa00e97e03b802e1d2a552cbd5ed325defdaa24
webdevkci <124649255+webdevkci@users.noreply.github.com>
2023-02-27 08:56:03 +0530

    learn object

--- HTML/JS/Object_JS/JS/index.js
+++ HTML/JS/Object_JS/JS/index.js
@@ -1,1 +1,58 @@
 console.log('Aj hum Object in java script padhenge');
+
+
+let a=5;
+
+let person = {
+
+    height:'5',
+    add:'colony roshan nagar',
+    mobile:'987456321',
+
+    cook:function() {
+        console.log('Can be cook');
+    }
+
+}; 
+
+
+let mobile = {
+
+    brand:'5',
+    dimenstion:'colony roshan nagar',
+    price:'987456321',
+
+    canCall:function() {
+        console.log('call Connected');
+    },
+    
+    canMassage:function() {
+        console.log('Massager sent' );
+    }
+
+}; 
+
+//object
+
+let laptop = {
+    // propertise
+    brand:'5',
+    dimenstion:'colony roshan nagar',
+    price:'987456321',
+
+
+    //behavior
+    canCall:function() {
+        console.log('call Connected');
+    },
+    
+    canMassage:function() {
+        console.log('Massager sent' );
+    }
+
+}; 
+
+
+
+
+
